2cf6e4b28d3a721eb00e5b13dd8ddaec84ee3a0f
Greg Emel <greg.emel@gmail.com>
2018-02-21 18:21:59 -0800

    added event handler

--- src/main/scala/controller/TemperatureEndpoint.scala
+++ src/main/scala/controller/TemperatureEndpoint.scala
@@ -1,19 +1,13 @@
 package controller
 
-import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
-import model.Temperature
-import repository.TemperatureRepository
-import spray.json.DefaultJsonProtocol._
-import spray.json.RootJsonFormat
+import handler.TemperatureEvents
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 
 object TemperatureEndpoint {
@@ -21,19 +15,18 @@ object TemperatureEndpoint {
   implicit val system: ActorSystem = ActorSystem()
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
-  implicit val tempFormat: RootJsonFormat[Temperature] = jsonFormat3(Temperature)
 
   def main(args: Array[String]) {
 
     val route: Route =
       get {
         pathPrefix("temperature" / LongNumber) { id =>
-          routeGetTemp(id)
+          TemperatureEvents.routeGetTemp(id)
         }
       } ~
         post {
           path("record-temp") {
-            routePostTemp
+            TemperatureEvents.routePostTemp
           }
         }
 
@@ -44,38 +37,4 @@ object TemperatureEndpoint {
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ ⇒ system.terminate()) // and shutdown when done
   }
-
-  def routePostTemp: Route = {
-    println("p0 pull the post data")
-    entity(as[Temperature]) { temp =>
-      println("p2 about to save")
-      val saved: Future[Done] = TemperatureRepository.saveTemperature(temp)
-      println("p4 write a temp record")
-      val doneIt = onComplete(saved) { done =>
-        val msg = s"p6 ($done) temp recorded! ($temp) log size=${TemperatureRepository.temperatureHistory.size}\n"
-        print(msg)
-        complete(msg)
-      }
-      print("p5 about to write new record...\n")
-      doneIt
-    }
-  }
-
-  def routeGetTemp(id: Long): Route = {
-    println(s"g1 get $id")
-    val futureMaybeTemperature: Future[Option[Temperature]] = TemperatureRepository.fetchTemperature(id)
-    println("g2 about to get...")
-    val success = onSuccess(futureMaybeTemperature) {
-      case Some(validId) =>
-        val msg = s"g5 yay, completed getting ($validId)\n"
-        println(msg)
-        complete(msg)
-      case None =>
-        println("g5 get failed to complete\n")
-        complete(StatusCodes.NotFound)
-    }
-    println("g4 waiting for maybe...")
-    success
-  }
-
 }
---
+++ src/main/scala/handler/TemperatureEvents.scala
@@ -0,0 +1,53 @@
+package handler
+
+import akka.Done
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import model.Temperature
+import repository.TemperatureRepository
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
+
+import scala.concurrent.Future
+
+object TemperatureEvents {
+
+  implicit val tempFormat: RootJsonFormat[Temperature] = jsonFormat3(Temperature)
+
+
+  def routePostTemp: Route = {
+    println("p0 pull the post data")
+    entity(as[Temperature]) { temp =>
+      println("p2 about to save")
+      val saved: Future[Done] = TemperatureRepository.saveTemperature(temp)
+      println("p4 write a temp record")
+      val doneIt = onComplete(saved) { done =>
+        val msg = s"p6 ($done) temp recorded! ($temp) log size=${TemperatureRepository.temperatureHistory.size}\n"
+        print(msg)
+        complete(msg)
+      }
+      print("p5 about to write new record...\n")
+      doneIt
+    }
+  }
+
+  def routeGetTemp(id: Long): Route = {
+    println(s"g1 get $id")
+    val futureMaybeTemperature: Future[Option[Temperature]] = TemperatureRepository.fetchTemperature(id)
+    println("g2 about to get...")
+    val success = onSuccess(futureMaybeTemperature) {
+      case Some(validId) =>
+        val msg = s"g5 yay, completed getting ($validId)\n"
+        println(msg)
+        complete(msg)
+      case None =>
+        println("g5 get failed to complete\n")
+        complete(StatusCodes.NotFound)
+    }
+    println("g4 waiting for maybe...")
+    success
+  }
+
+}
